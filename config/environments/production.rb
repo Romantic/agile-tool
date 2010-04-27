4b211c8b307f82031e15f4b93ca0c1a64b6be80f
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2010-04-28 02:10:18 +0300

    Adds host to production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,9 @@ AgileTool::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'agile-tool.heroku.com' }
+
   # Enable threaded mode
   # config.threadsafe!
 end
+
